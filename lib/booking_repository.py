f1232f7a26ec72d6a51286cdf2d8946629827bfd
tom england <tomengland1995@gmail.com>
2024-09-10 15:26:18 +0100

    bookings

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -11,10 +11,13 @@ class BookingRepository():
             all_bookings.append(item)
         return all_bookings
     
-    # def all(self):
-    #     rows = self._connection.execute('SELECT * from artists')
-    #     artists = []
-    #     for row in rows:
-    #         item = Artist(row["id"], row["name"], row["genre"])
-    #         artists.append(item)
-    #     return artists
+    def create(self, booking):
+        self._connection.execute('INSERT INTO bookings (date_booked, booking_status, user_id, space_id) VALUES (%s, %s, %s, %s)', [
+                                booking.date_booked, booking.booking_status, booking.user_id, booking.space_id])
+        return None
+
+    def confirm_booking(self, id):
+        self._connection.execute(
+        'UPDATE bookings SET booking_status = %s WHERE id = %s', 
+        ['confirmed', id])
+        return None
--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -12,13 +12,18 @@ def test_read_bookings(db_connection):
 
     
     assert all_bookings == [Booking(1, date(2024,9,25), 'confirmed', 1, 3), Booking(2, date(2024,11,23), 'confirmed', 1, 5), Booking(3, date(2025,9,20), 'pending', 2, 1), Booking(4, date(2024,10,15), 'pending', 2, 4), Booking(5, date(2024,12,30), 'pending', 1, 2)]
-# def test_get_all_records(db_connection): # See conftest.py to learn what `db_connection` is.
-#     db_connection.seed("seeds/music_library.sql") # Seed our database with some test data
-#     repository = AlbumRepository(db_connection) # Create a new AlbumRepository 
 
-#     albums = repository.all() # Get all albums
-
-#     # Assert on the results
-#     assert albums == [Album(1, 'Doolittle', 1989, 1), Album(2, 'Surfer Rosa', 1988, 1), Album(3, 'Waterloo', 1974, 2), Album(4, 'Super Trouper', 1980, 2), Album(5, 'Bossanova', 1990, 1), Album(6, 'Lover', 2019, 3), Album(7, 'Folklore', 2020, 3), Album(8, 'I Put a Spell on You', 1965, 4), Album(9, 'Baltimore', 1978, 4), Album(10, 'Here Comes the Sun', 1971, 4), Album(11, 'Fodder on My Wings', 1982, 4), Album(12, 'Ring Ring', 1973, 2)]
-
-# """
+def test_create_booking(db_connection):
+    db_connection.seed('seeds/air_makersbnb_test.sql')
+    bookings = BookingRepository(db_connection)
+    new_booking = bookings.create(Booking(None, date(2024,9,25), 'confirmed', 1, 3))
+    
+    assert bookings.all() == [Booking(1, date(2024,9,25), 'confirmed', 1, 3), Booking(2, date(2024,11,23), 'confirmed', 1, 5), Booking(3, date(2025,9,20), 'pending', 2, 1), Booking(4, date(2024,10,15), 'pending', 2, 4), Booking(5, date(2024,12,30), 'pending', 1, 2), Booking(6, date(2024,9,25), 'confirmed', 1, 3)]
+        
+def test_update_booking(db_connection):
+    db_connection.seed('seeds/air_makersbnb_test.sql')
+    new = BookingRepository(db_connection)
+    new.confirm_booking(3) 
+    # bookings = BookingRepository(db_connection)
+    
+    assert new.all() == [Booking(1, date(2024,9,25), 'confirmed', 1, 3), Booking(2, date(2024,11,23), 'confirmed', 1, 5), Booking(3, date(2025,9,20), 'confirmed', 2, 1), Booking(4, date(2024,10,15), 'pending', 2, 4), Booking(5, date(2024,12,30), 'pending', 1, 2)]
