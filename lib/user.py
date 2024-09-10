6a3433e29a5a9f6ef58ce223baab672a7989da8a
Avnita <akbhandal@gmail.com>
2024-09-10 12:01:39 +0100

    Edit to model class to add spaces and bookings

--- lib/user.py
+++ lib/user.py
@@ -1,10 +1,12 @@
 class User:
 
-    def __init__(self, id, username, email, password):
+    def __init__(self, id, username, email, password, spaces = [], bookings = []):
         self.id = id
         self.username = username
         self.email = email
         self.password = password
+        self.spaces = spaces 
+        self.bookings = bookings
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
--- tests/test_user.py
+++ tests/test_user.py
@@ -10,6 +10,8 @@ def test_user_constructs_unique_username():
     assert user.username == "Avnita"
     assert user.email == "ab@example.com"
     assert user.password == "Password£123"
+    assert user.spaces == []
+    assert user.bookings == []
     # try:
     #     user2 = User(2, "Avnita", "po@example.com", "kjfein%121")
     # except Exception as excinfo:
@@ -19,7 +21,7 @@ def test_user_constructs_unique_username():
 We can format user to strings nicely
 """
 def test_user_format_nicely():
-    user = User(1, "Avnita", "ab@example.com", "Password£123")
+    user = User(1, "Avnita", "ab@example.com", "Password£123", [], [])
     assert str(user) == "User(1, Avnita, ab@example.com, Password£123)"
 
 
@@ -28,6 +30,6 @@ We can compare two identical users
 And have them be equal
 """
 def test_users_are_equal():
-    user1 = User(1, "Avnita", "ab@example.com", "Password£123")
-    user2 = User(1, "Avnita", "ab@example.com", "Password£123")
+    user1 = User(1, "Avnita", "ab@example.com", "Password£123", [], [])
+    user2 = User(1, "Avnita", "ab@example.com", "Password£123", [], [])
     assert user1 == user2
