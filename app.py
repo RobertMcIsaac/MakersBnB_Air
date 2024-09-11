fb49005a41e61f90e8efb771929e626406efaa5c
tom england <tomengland1995@gmail.com>
2024-09-11 15:58:27 +0100

    redirect

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
 from lib.booking_repository import BookingRepository
 from lib.booking import Booking
